0a707a9566381ffa12c239815224a557a56aba21
ANSH GOYAL <72507723+Ansh1693@users.noreply.github.com>
2022-11-05 17:30:36 +0530

    Added Navbar

--- brevet/src/App.js
+++ brevet/src/App.js
@@ -1,9 +1,17 @@
 // import logo from "./logo.svg";
 import Home from "./components/home";
+import Navbar from "./components/Navbar";
+import footer from "./components/footer";
+
 import "./App.css";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
 }
 
 export default App;
---
+++ brevet/src/components/footer.js
@@ -0,0 +1,15 @@
+import "../css/footer.css";
+import React from "react";
+
+export default function footer() {
+  return (
+    <div className="footer">
+      <div className="footer-item">
+        <span>01</span>
+      </div>
+      <div className="footer-item">
+        <span> Built with 💖 in India</span>
+      </div>
+    </div>
+  );
+}
