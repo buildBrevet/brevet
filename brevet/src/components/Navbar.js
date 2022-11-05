2b845b84d81f8bcb96b5825fdbf227772aa35c49
ANSH GOYAL <72507723+Ansh1693@users.noreply.github.com>
2022-11-05 19:12:49 +0530

    Address

--- brevet/src/components/Navbar.js
+++ brevet/src/components/Navbar.js
@@ -43,7 +43,9 @@ import "../css/Navbar.css";
 
 export default function Navbar() {
   const targetRef = React.useRef();
-  const [meta, setMeta] = React.useState(1);
+  const [meta, setMeta] = React.useState(0);
+  const [meta1, setMeta1] = React.useState(1);
+
   //   const callback = (entries) => {
   //     const [entry] = entries;
   //     if (!entry.isIntersecting) {
@@ -68,6 +70,29 @@ export default function Navbar() {
   //       if (currentTarget) observer.unobserve(currentTarget);
   //     };
   //   }, [targetRef, options]);
+
+  React.useEffect(() => {
+    // metamask();
+  }, []);
+
+  function metamask() {
+    if (meta) {
+      return (
+        <div className="Nav-meta">
+          <div>
+            <span>Connect your metamask wallet</span>
+          </div>
+          <button className="Nav-meta-button">
+            <span>Connect your meta mask wallet</span>
+          </button>
+        </div>
+      );
+    } else return;
+  }
+
+  //   function address(){
+  //     if(meta1)
+  //   }
 
   return (
     <>
@@ -227,13 +252,27 @@ export default function Navbar() {
           </div>
         </div>
       </div>
-      <div className="Nav-meta">
-        <div>
-          <span>Connect your metamask wallet</span>
-        </div>
-        <button className="Nav-meta-button">
-          <span>Connect your meta mask wallet</span>
-        </button>
+      {/* {metamask()} */}
+      <div className="nav-model">
+        <span className="nav-address">
+          0xCb19180B1D34eFDfd02899C21Feff15998bA1687
+        </span>
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#999999"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
+            />
+          </svg>
+        </span>
       </div>
     </>
   );
