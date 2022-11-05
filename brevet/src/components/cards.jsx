eb944e0bcfe8d3d8271d1dd20b7033d6d570e84f
Divyansh3021 <divyansh3021@gmail.com>
2022-11-05 17:40:48 +0530

    marketplace cards added

---
+++ brevet/src/components/cards.jsx
@@ -0,0 +1,110 @@
+import "../css/cards.css"
+import React from 'react'
+
+function Cards() {
+  return (
+    <div className="car-cards-con">
+        <div className="car-card">
+            {/* <div className="car-btns"> */}
+                <a href="" className="car-link"><img src={require("../components/Assets/buy.png")} alt="" className="car-link-img" /></a>
+                <a href="" className="car-link"><img src={require("../components/Assets/info.png")} alt="" className="car-link-img" /></a>
+            {/* </div> */}
+            {/* <div className="car-bg-img-con">
+                <img src={require("../components/Assets/board-ape-bg.webp")} className="car-bg-img" alt="" />
+            </div> */}
+            <div className="car-card-info">
+                <h2 className="car-coll-name">Collection Name</h2>
+                <h3 className="car-coll-val">0.05 ETH</h3>
+            </div>
+        </div>
+        
+        <div className="car-card">
+            {/* <div className="car-btns"> */}
+                <a href="" className="car-link"><img src={require("../components/Assets/buy.png")} alt="" className="car-link-img" /></a>
+                <a href="" className="car-link"><img src={require("../components/Assets/info.png")} alt="" className="car-link-img" /></a>
+            {/* </div> */}
+            {/* <div className="car-bg-img-con">
+                <img src={require("../components/Assets/board-ape-bg.webp")} className="car-bg-img" alt="" />
+            </div> */}
+            <div className="car-card-info">
+                <h2 className="car-coll-name">Collection Name</h2>
+                <h3 className="car-coll-val">0.05 ETH</h3>
+            </div>
+        </div>
+        
+        <div className="car-card">
+            {/* <div className="car-btns"> */}
+                <a href="" className="car-link"><img src={require("../components/Assets/buy.png")} alt="" className="car-link-img" /></a>
+                <a href="" className="car-link"><img src={require("../components/Assets/info.png")} alt="" className="car-link-img" /></a>
+            {/* </div> */}
+            {/* <div className="car-bg-img-con">
+                <img src={require("../components/Assets/board-ape-bg.webp")} className="car-bg-img" alt="" />
+            </div> */}
+            <div className="car-card-info">
+                <h2 className="car-coll-name">Collection Name</h2>
+                <h3 className="car-coll-val">0.05 ETH</h3>
+            </div>
+        </div>
+        
+        <div className="car-card">
+            {/* <div className="car-btns"> */}
+                <a href="" className="car-link"><img src={require("../components/Assets/buy.png")} alt="" className="car-link-img" /></a>
+                <a href="" className="car-link"><img src={require("../components/Assets/info.png")} alt="" className="car-link-img" /></a>
+            {/* </div> */}
+            {/* <div className="car-bg-img-con">
+                <img src={require("../components/Assets/board-ape-bg.webp")} className="car-bg-img" alt="" />
+            </div> */}
+            <div className="car-card-info">
+                <h2 className="car-coll-name">Collection Name</h2>
+                <h3 className="car-coll-val">0.05 ETH</h3>
+            </div>
+        </div>
+        
+        <div className="car-card">
+            {/* <div className="car-btns"> */}
+                <a href="" className="car-link"><img src={require("../components/Assets/buy.png")} alt="" className="car-link-img" /></a>
+                <a href="" className="car-link"><img src={require("../components/Assets/info.png")} alt="" className="car-link-img" /></a>
+            {/* </div> */}
+            {/* <div className="car-bg-img-con">
+                <img src={require("../components/Assets/board-ape-bg.webp")} className="car-bg-img" alt="" />
+            </div> */}
+            <div className="car-card-info">
+                <h2 className="car-coll-name">Collection Name</h2>
+                <h3 className="car-coll-val">0.05 ETH</h3>
+            </div>
+        </div>
+        
+        <div className="car-card">
+            {/* <div className="car-btns"> */}
+                <a href="" className="car-link"><img src={require("../components/Assets/buy.png")} alt="" className="car-link-img" /></a>
+                <a href="" className="car-link"><img src={require("../components/Assets/info.png")} alt="" className="car-link-img" /></a>
+            {/* </div> */}
+            {/* <div className="car-bg-img-con">
+                <img src={require("../components/Assets/board-ape-bg.webp")} className="car-bg-img" alt="" />
+            </div> */}
+            <div className="car-card-info">
+                <h2 className="car-coll-name">Collection Name</h2>
+                <h3 className="car-coll-val">0.05 ETH</h3>
+            </div>
+        </div>
+        
+        <div className="car-card">
+            {/* <div className="car-btns"> */}
+                <a href="" className="car-link"><img src={require("../components/Assets/buy.png")} alt="" className="car-link-img" /></a>
+                <a href="" className="car-link"><img src={require("../components/Assets/info.png")} alt="" className="car-link-img" /></a>
+            {/* </div> */}
+            {/* <div className="car-bg-img-con">
+                <img src={require("../components/Assets/board-ape-bg.webp")} className="car-bg-img" alt="" />
+            </div> */}
+            <div className="car-card-info">
+                <h2 className="car-coll-name">Collection Name</h2>
+                <h3 className="car-coll-val">0.05 ETH</h3>
+            </div>
+        </div>
+        
+        
+    </div>
+  )
+}
+
+export default Cards
